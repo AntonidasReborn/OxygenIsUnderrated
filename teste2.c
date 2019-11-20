6ac1c44892def708da5edc01f61e3487be19408e
flavio junior <fjcvj@cin.ufpe.br>
2019-11-19 21:58:57 -0300

    flavio made another one,pra compilar -> gcc teste2.c -o a -lallegro -lallegro_image -lallegro_dialog -lallegro_audio -lallegro_acodec -lallegro_font -lallegro_ttf

--- teste2.c
+++ teste2.c
@@ -1,13 +1,56 @@
 // Os arquivos de cabeçalho
+
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_native_dialog.h>
 #include <allegro5/allegro_image.h> 
 #include <allegro5/allegro_audio.h>
 #include <allegro5/allegro_acodec.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_ttf.h>
 // Atributos da tela
 #define LARGURA_TELA 800
 #define ALTURA_TELA 600
- 
+#define BUFFER_SIZE 100
+#define ASTRBR 1
+#define ASTRURSS 2
+#define ASTRMESSI 3
+#define ASTRDEFAULT 4
+#define LOGIN_MAX_SIZE 13
+
+ void readInput(ALLEGRO_EVENT event, char str[], int limit)
+{
+    if (event.type == ALLEGRO_EVENT_KEY_CHAR)
+    {
+        if (strlen(str) <= limit) 
+        {
+            char temp[] = {event.keyboard.unichar, '\0'};
+            if (event.keyboard.unichar == ' ')
+            {
+                strcat(str, temp);
+            }
+            else if (event.keyboard.unichar >= '!' &&
+                     event.keyboard.unichar <= '?')
+            {
+                strcat(str, temp);
+            }
+            else if (event.keyboard.unichar >= 'A' &&
+                     event.keyboard.unichar <= 'Z')
+            {
+                strcat(str, temp);
+            }
+            else if (event.keyboard.unichar >= 'a' &&
+                     event.keyboard.unichar <= 'z')
+            {
+                strcat(str, temp);
+            }
+        }
+
+        if (event.keyboard.keycode == ALLEGRO_KEY_BACKSPACE && strlen(str) != 0)
+        {
+            str[strlen(str) - 1] = '\0';
+        }
+    }
+}
 void error_msg(char *text){
 	al_show_native_message_box(NULL,"ERRO",
 		"Ocorreu o seguinte erro e o programa sera finalizado:",
@@ -15,14 +58,19 @@ void error_msg(char *text){
 }
  
 int main(void){
+    char ServerIP[30] = {"127.0.0.1"};
+    //Salva o login
+    char loginMsg[BUFFER_SIZE]={0};
     ALLEGRO_DISPLAY *janela = NULL;
+    ALLEGRO_FONT *font = NULL;
     ALLEGRO_EVENT_QUEUE *fila_eventos = NULL;
     ALLEGRO_BITMAP *botao_sair = NULL, *botao_jogar=NULL,*botao_historia=NULL,*botao_tutorial=NULL,*area_central=0;
-    ALLEGRO_BITMAP *background =NULL,*backgroundHistoria=NULL,*backgroundTutorial=NULL;
+    ALLEGRO_BITMAP *background =NULL,*backgroundHistoria=NULL,*backgroundTutorial=NULL,*backgroundIP=NULL,*backgroundLogin=NULL,*backgroundCharacter=NULL;
     ALLEGRO_BITMAP *imagem =NULL;
+    ALLEGRO_BITMAP *astrURSS=NULL,*astrBR=NULL,*astrMessi=NULL,*astrDefault=NULL;
     ALLEGRO_AUDIO_STREAM *musica =NULL;
     // Flag que condicionará nosso looping
-    int sair = 0,historia = 0,tutorial=0;
+    int sair = 0,historia = 0,tutorial=0,jogar=0,telaIp=0,telaLogin=0,telaCharacter=0;
     if(!al_install_audio()){
         error_msg("Falha ao inicializar o audio");
         return 0;
@@ -45,9 +93,22 @@ int main(void){
         error_msg("Falha ao reservar amostrar de audio");
         return 0;
     }
+    
+    al_init_font_addon();
+
+    if (!al_init_ttf_addon()){
+        error_msg("Falha ao inicializar add-on allegro_ttf");
+        return -1;
+    }
     janela = al_create_display(LARGURA_TELA, ALTURA_TELA);
     if (!janela){
         error_msg("Falha ao criar janela");
+        return -1;
+    }
+    font = al_load_font("Resource/font/Minecraft.ttf", 68, 0);
+    if (!font){
+        al_destroy_display(janela);
+        error_msg("Falha ao carregar fonte");
         return -1;
     }
     
@@ -73,22 +134,65 @@ int main(void){
         al_destroy_display(janela);
         return -1;
     }
+    astrBR = al_load_bitmap("Resource/skins/AstrounautaBR.png");
+    if (!astrBR){
+        error_msg("Falha ao criar bitmap");
+        al_destroy_display(janela);
+        return -1;
+    }
+    astrURSS = al_load_bitmap("Resource/skins/AstrounautinhaURSS.png");
+    if (!astrURSS){
+        error_msg("Falha ao criar bitmap");
+        al_destroy_display(janela);
+        return -1;
+    }
+    astrMessi = al_load_bitmap("Resource/skins/AstrounautaARGENTINA.png");
+    if (!astrMessi){
+        error_msg("Falha ao criar bitmap");
+        al_destroy_display(janela);
+        return -1;
+    }
+    astrDefault = al_load_bitmap("Resource/skins/Astrounautinha.png");
+    if (!astrDefault){
+        error_msg("Falha ao criar bitmap");
+        al_destroy_display(janela);
+        return -1;
+    }
+    
+    backgroundCharacter = al_load_bitmap("Resource/background/Backgrounds_800x600/EscolhaPersonagem_background.png");
+    if (!backgroundCharacter){
+        error_msg("Falha ao criar bitmap");
+        al_destroy_display(janela);
+        return -1;
+    }
     backgroundHistoria = al_load_bitmap("Resource/background/Backgrounds_800x600/Game_Hist.png");
     if (!backgroundHistoria){
-        error_msg("Falha ao criar backgroundHistoria");
+        error_msg("Falha ao criar bitmap");
         al_destroy_display(janela);
         return -1;
     }
     backgroundTutorial = al_load_bitmap("Resource/background/Backgrounds_800x600/Game_Tutorial.png");
     if (!backgroundTutorial){
-        error_msg("Falha ao criar backgroundTutorial");
+        error_msg("Falha ao criar bitmap");
         al_destroy_display(janela);
         return -1;
     }
     // Alocamos o retângulo central da tela
     background = al_load_bitmap("Resource/background/Backgrounds_800x600/spacebackground_MENU2.jpg");
     if (!background){
-        error_msg("Falha ao criar background");
+        error_msg("Falha ao criar bitmap");
+        al_destroy_display(janela);
+        return -1;
+    }
+     backgroundIP = al_load_bitmap("Resource/background/Backgrounds_800x600/DigiteIP_background.png");
+    if (!background){
+        error_msg("Falha ao criar bitmap");
+        al_destroy_display(janela);
+        return -1;
+    }
+    backgroundLogin = al_load_bitmap("Resource/background/Backgrounds_800x600/DigiteLogin_background.png");
+    if (!background){
+        error_msg("Falha ao criar bitmap");
         al_destroy_display(janela);
         return -1;
     }
@@ -210,8 +314,80 @@ int main(void){
                 evento.mouse.y >= ALTURA_TELA - al_get_bitmap_height(botao_tutorial) - 10){
                     tutorial = 1;
                 }
+                if (evento.mouse.x >= LARGURA_TELA - al_get_bitmap_width(botao_jogar) - 550 &&
+                evento.mouse.x <= LARGURA_TELA - 550 && evento.mouse.y <= ALTURA_TELA - 150 &&
+                evento.mouse.y >= ALTURA_TELA - al_get_bitmap_height(botao_jogar) - 150){
+                    jogar = 1;
+                }
             }
-        }   
+        }
+        while(jogar==1){
+            while (!al_is_event_queue_empty(fila_eventos)){
+            ALLEGRO_EVENT evento;
+            al_wait_for_event(fila_eventos, &evento);
+             readInput(evento,ServerIP,20);
+            if(evento.type == ALLEGRO_EVENT_KEY_DOWN){
+                switch (evento.keyboard.keycode){
+                    case ALLEGRO_KEY_ENTER:
+                        telaLogin=1;
+                        jogar=0;
+                }
+            }
+            if(evento.type == ALLEGRO_EVENT_DISPLAY_CLOSE){
+	                sair=1;
+	            }
+        }
+             al_draw_bitmap(backgroundIP, LARGURA_TELA - al_get_bitmap_width(backgroundIP),
+        ALTURA_TELA  - al_get_bitmap_height(backgroundIP), 0);
+
+        if (strlen(ServerIP) > 0)
+    {
+        al_draw_text(font, al_map_rgb(255, 255, 255), LARGURA_TELA / 2,
+                     (ALTURA_TELA - al_get_font_ascent(font)) / 2,
+                     ALLEGRO_ALIGN_CENTRE, ServerIP);
+    }
+    else
+    {
+        al_draw_text(font, al_map_rgb(255, 255, 255), LARGURA_TELA / 2,
+                     (ALTURA_TELA - al_get_font_ascent(font)) / 2,
+                     ALLEGRO_ALIGN_CENTRE, "0.0.0.0");
+    }
+         al_flip_display();    
+    }
+         while(telaLogin==1){
+            while (!al_is_event_queue_empty(fila_eventos)){
+            ALLEGRO_EVENT evento;
+            al_wait_for_event(fila_eventos, &evento);
+            readInput(evento,loginMsg,LOGIN_MAX_SIZE);
+            if(evento.type == ALLEGRO_EVENT_KEY_DOWN){
+                switch (evento.keyboard.keycode){
+                    case ALLEGRO_KEY_ENTER:
+                        telaCharacter=1;
+                        telaLogin=0;
+                }
+            }
+            if(evento.type == ALLEGRO_EVENT_DISPLAY_CLOSE){
+	                sair=1;
+	            }
+        }
+             al_draw_bitmap(backgroundLogin, LARGURA_TELA - al_get_bitmap_width(backgroundLogin),
+        ALTURA_TELA  - al_get_bitmap_height(backgroundLogin), 0);
+
+        if (strlen(loginMsg) > 0)
+    {
+        al_draw_text(font, al_map_rgb(255, 255, 255), LARGURA_TELA / 2,
+                     (ALTURA_TELA - al_get_font_ascent(font)) / 2,
+                     ALLEGRO_ALIGN_CENTRE, loginMsg);
+    }
+    else
+    {
+        al_draw_text(font, al_map_rgb(255, 255, 255), LARGURA_TELA / 2,
+                     (ALTURA_TELA - al_get_font_ascent(font)) / 2,
+                     ALLEGRO_ALIGN_CENTRE, "<login>");
+    }
+        
+         al_flip_display();    
+    }         
         while(historia==1){
             while (!al_is_event_queue_empty(fila_eventos)){
             ALLEGRO_EVENT evento;
@@ -226,6 +402,63 @@ int main(void){
              al_draw_bitmap(backgroundHistoria, LARGURA_TELA - al_get_bitmap_width(backgroundHistoria),
         ALTURA_TELA  - al_get_bitmap_height(backgroundHistoria), 0);
         
+         al_flip_display();    
+    }
+    int personagem_exibido = ASTRBR;
+     while(telaCharacter==1){
+            while (!al_is_event_queue_empty(fila_eventos)){
+            ALLEGRO_EVENT evento;
+            al_wait_for_event(fila_eventos, &evento);
+            if(evento.type == ALLEGRO_EVENT_KEY_DOWN){
+                switch (evento.keyboard.keycode){
+                    case ALLEGRO_KEY_RIGHT:
+                         personagem_exibido+=1;
+							personagem_exibido%=5;
+							if(personagem_exibido == 0){
+								personagem_exibido++;
+							}
+                        break;
+                    case ALLEGRO_KEY_LEFT:
+                        personagem_exibido-=1;
+							if(personagem_exibido == 0){
+								personagem_exibido+=4;
+							}
+                        break;
+                    
+                        
+                }
+            }
+        }
+         al_draw_bitmap(backgroundCharacter, LARGURA_TELA - al_get_bitmap_width(backgroundCharacter),
+        ALTURA_TELA  - al_get_bitmap_height(backgroundCharacter), 0);
+        if(personagem_exibido==1){
+            al_draw_bitmap(astrBR, LARGURA_TELA - al_get_bitmap_width(astrBR)-340,
+            ALTURA_TELA  - al_get_bitmap_height(astrBR)-220, 0);
+            al_draw_text(font, al_map_rgb(255, 255, 255), LARGURA_TELA- al_get_font_ascent(font)-340,
+                     ALTURA_TELA - al_get_font_ascent(font)-150,
+                     ALLEGRO_ALIGN_CENTRE, "BRASIL");
+        }
+        if(personagem_exibido==2){
+           al_draw_bitmap(astrURSS, LARGURA_TELA - al_get_bitmap_width(astrURSS)-340,
+        ALTURA_TELA  - al_get_bitmap_height(astrURSS)-220, 0);
+        al_draw_text(font, al_map_rgb(255, 255, 255), LARGURA_TELA- al_get_font_ascent(font)-340,
+                     ALTURA_TELA - al_get_font_ascent(font)-150,
+                     ALLEGRO_ALIGN_CENTRE, "URSS");
+        }
+        if(personagem_exibido==3){
+           al_draw_bitmap(astrMessi, LARGURA_TELA - al_get_bitmap_width(astrMessi)-340,
+        ALTURA_TELA  - al_get_bitmap_height(astrMessi)-220, 0);
+        al_draw_text(font, al_map_rgb(255, 255, 255), LARGURA_TELA- al_get_font_ascent(font)-340,
+                     ALTURA_TELA - al_get_font_ascent(font)-150,
+                     ALLEGRO_ALIGN_CENTRE, "ARGENTINA");
+        }
+        if(personagem_exibido==4){
+            al_draw_bitmap(astrDefault, LARGURA_TELA - al_get_bitmap_width(astrDefault)-340,
+        ALTURA_TELA  - al_get_bitmap_height(astrDefault)-220, 0);
+        al_draw_text(font, al_map_rgb(255, 255, 255), LARGURA_TELA- al_get_font_ascent(font)-340,
+                     ALTURA_TELA - al_get_font_ascent(font)-150,
+                     ALLEGRO_ALIGN_CENTRE, "DEFAULT");
+        }
          al_flip_display();    
     }
      while(tutorial==1){
@@ -291,6 +524,7 @@ int main(void){
     al_destroy_bitmap(botao_tutorial);
     al_destroy_bitmap(background);
     al_destroy_display(janela);
+    al_destroy_font(font);
     al_destroy_event_queue(fila_eventos);
      al_destroy_audio_stream(musica);
     return 0;
